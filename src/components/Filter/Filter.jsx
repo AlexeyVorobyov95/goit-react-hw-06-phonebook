3bb37bb897f123286f512dc775029778b9ecf065
AlexeyVorobyov95 <forced1395@gmail.com>
2023-04-19 20:19:03 +0300

    Add fix filters

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,11 +1,9 @@
 import { FilterContainer, Input } from 'components/Filter/Filter.styled';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { setStatusFilter } from 'redux/filtersSlice';
-import { getStatusFilter } from 'redux/selectors';
 
 export const Filter = () => {
   const dispatch = useDispatch();
-  const filter = useSelector(getStatusFilter);
 
   const handleFilter = evt => {
     dispatch(setStatusFilter(evt.target.value));
@@ -14,7 +12,7 @@ export const Filter = () => {
     <FilterContainer>
       <h2>Finde contact</h2>
       <label htmlFor="">
-        <Input type="text" value={filter} onChange={handleFilter} />
+        <Input type="text" onChange={handleFilter} />
       </label>
     </FilterContainer>
   );
